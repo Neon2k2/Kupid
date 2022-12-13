ee57c359170e1513bab6d875d7a4b02d80a47539
Neon2k2 <gaurishninnekar@gmail.com>
2022-12-14 00:18:04 +0530

    7th commit

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-lists',
@@ -10,6 +12,9 @@ export class ListsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  
 
 }
